038857ff027e52a9eb36b992760bb6f83725eaac
sanma1i <51243829+sanma1i@users.noreply.github.com>
2019-09-11 15:46:58 -0400

    added files

---
+++ client/src/Context.js
@@ -0,0 +1,9 @@
+// import React, {
+//     Component
+// } from 'react';
+// import Data from './Data';
+
+// constructor() {
+//     super();
+//     this.data = new Data();
+// }
---
+++ client/src/Data.js
@@ -0,0 +1,17 @@
+export default class Data {
+    api(path, method, body = null) {
+        const url = config.apiBaseUrl + path;
+
+        const options = {
+            method,
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+        };
+
+        if (body !== null) {
+            options.body = JSON.stringify(body);
+        }
+        return fetch(url, options);
+    }
+}
