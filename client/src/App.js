210660910ab72869dea986e27764ac851c0bf2ff
sanma1i <51243829+sanma1i@users.noreply.github.com>
2019-09-12 10:02:55 -0400

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// // import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch,
+// } from 'react-router-dom';
+//import withContext from './Context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const HeaderWithContext = withContext(Header);
+
+// export default () => (
+//   <Router>
+//     <div>
+//       <HeaderWithContext />
+//     </div>
+//   </Router>
+// );
+
+class App extends React.Component {
+  state = {
+    courses: [],
+    isLoading: true
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:5000/api/courses`)
+      .then(response => response.json())
+      .then(courses => this.setState({
+        courses: courses,
+        isLoading: false,
+      }))
+      .then(() => console.log(this.state.courses))
+      .catch(error => console.log('Sorry'));
+  }
+  render() {
+    return this.state.isLoading ? (<h2>It's Loading...</h2>) : (
+      <div className="container">
+        {
+          this.state.courses.map(course => {
+            return (
+              <ul key={course.id}>
+                <li>{course.id}</li>
+                <li>{course.title}</li>
+                <li>{course.description}</li>
+                <li>{course.estimatedTime}</li>
+                <li>{course.materialsNeeded}</li>
+              </ul>
+            );
+          })
+        }
+      </div>
+    )
+  }
 }
-
 export default App;
+
