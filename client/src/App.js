0c9feafc21b7129e86e7a3456a264793a97c63d0
sanma1i <51243829+sanma1i@users.noreply.github.com>
2019-09-12 11:29:52 -0400

    Importing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,59 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// // import {
-//   BrowserRouter as Router,
-//   Route,
-//   Switch,
-// } from 'react-router-dom';
-//import withContext from './Context';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-// const HeaderWithContext = withContext(Header);
+//Import Components
+import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import Header from './components/Header';
+import UserSignOut from './components/UserSignOut'
+import NotFound from './components/NotFound';
+import Forbidden from './components/Forbidden';
+import UnhandledError from './components/UnhendledError';
 
-// export default () => (
-//   <Router>
-//     <div>
-//       <HeaderWithContext />
-//     </div>
-//   </Router>
-// );
 
 class App extends React.Component {
-  state = {
-    courses: [],
-    isLoading: true
-  }
+  //   state = {
+  //     courses: [],
+  //     isLoading: true
+  //   }
 
-  componentDidMount() {
-    fetch(`http://localhost:5000/api/courses`)
-      .then(response => response.json())
-      .then(courses => this.setState({
-        courses: courses,
-        isLoading: false,
-      }))
-      .then(() => console.log(this.state.courses))
-      .catch(error => console.log('Sorry'));
-  }
-  render() {
-    return this.state.isLoading ? (<h2>It's Loading...</h2>) : (
-      <div className="container">
-        {
-          this.state.courses.map(course => {
-            return (
-              <ul key={course.id}>
-                <li>{course.id}</li>
-                <li>{course.title}</li>
-                <li>{course.description}</li>
-                <li>{course.estimatedTime}</li>
-                <li>{course.materialsNeeded}</li>
-              </ul>
-            );
-          })
-        }
-      </div>
-    )
-  }
+  //   componentDidMount() {
+  //     fetch(`http://localhost:5000/api/courses`)
+  //       .then(response => response.json())
+  //       .then(courses => this.setState({
+  //         courses: courses,
+  //         isLoading: false,
+  //       }))
+  //       .then(() => console.log(this.state.courses))
+  //       .catch(error => console.log('Sorry'));
+  //   }
+  //   render() {
+  //     return this.state.isLoading ? (<h2>It's Loading...</h2>) : (
+  //       <div className="container">
+  //         {
+  //           this.state.courses.map(course => {
+  //             return (
+  //               <ul key={course.id}>
+  //                 <li>{course.id}</li>
+  //                 <li>{course.title}</li>
+  //                 <li>{course.description}</li>
+  //                 <li>{course.estimatedTime}</li>
+  //                 <li>{course.materialsNeeded}</li>
+  //               </ul>
+  //             );
+  //           })
+  //         }
+  //       </div>
+  //     )
+  //   }
 }
 export default App;
 
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//This componenet displays a message if there is no results for the search
+const NotFound = () => {
+    return (
+        <div className="Not Found">
+            <h3> "No results found" </h3>
+            <p> "404 error.Page not found." </p>
+        </div>)
+}
+export default NotFound;
